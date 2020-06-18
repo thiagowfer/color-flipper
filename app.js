68cd544682688bfa38d6cc9be668e1460c038955
thiagowfer <thiagowfer@gmail.com>
2020-06-17 22:55:15 -0300

    Finishing the project

---
+++ app.js
@@ -0,0 +1,17 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "F"];
+const btn = document.querySelector("#btn");
+const color = document.querySelector('.color');
+
+btn.addEventListener("click", function() {
+    let hexColor = "#";
+    for(let i = 0; i < 6; i++){
+        hexColor += getRandomNumber();
+    }
+
+    color.textContent = hexColor;
+    document.body.style.background = hexColor;
+});
+
+function getRandomNumber(){
+    return hex[Math.floor(Math.random() * hex.length)];
+};
